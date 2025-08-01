6a5d9722a6bdef87556f6a807ef9297250f3f74e
Anbarasan-P <anbarasanpandikumar@gmail.com>
2025-08-01 14:07:10 +0530

    Add AuthController with JWT token generation for user login

---
+++ Trainee-webAPI/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trainee_webAPI.Models;
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly TraineeRepository _dataAccessLayer;
+    private readonly JwtService _jsonWebToken;
+
+    public AuthController(IConfiguration config)
+    {
+        _dataAccessLayer = new TraineeRepository(config);
+        _jsonWebToken = new JwtService(config);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginModel login)
+    {
+        var user = _dataAccessLayer.GetByEmail(login.Email);
+
+        if (user == null || user.Password != login.Password)
+            return Unauthorized("Invalid credentials");
+
+        var token = _jsonWebToken.GenerateToken(user.Email);
+        return Ok(new { Token = token });
+    }
+}
+
