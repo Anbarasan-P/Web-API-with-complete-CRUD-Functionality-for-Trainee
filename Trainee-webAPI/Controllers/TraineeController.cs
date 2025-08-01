8644d6e272760377b4d1a9ba377c0b6bd955401b
Anbarasan-P <anbarasanpandikumar@gmail.com>
2025-08-01 14:07:37 +0530

    Add TraineeController with secured CRUD endpoints using JWT authorization

---
+++ Trainee-webAPI/Controllers/TraineeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Trainee_webAPI.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TraineeController : ControllerBase
+{
+    private readonly TraineeRepository dataAccessLayer;
+
+    public TraineeController(IConfiguration config)
+    {
+        dataAccessLayer = new TraineeRepository(config);
+    }
+
+    [HttpGet("all")]
+    [Authorize]
+    public IActionResult GetAll()
+    {
+        return Ok(dataAccessLayer.GetAll());
+    }
+
+    [HttpPost("create")]
+    public IActionResult Create([FromBody] Trainee trainee)
+    {
+        dataAccessLayer.Create(trainee);
+        return Ok("Created");
+    }
+
+    [HttpPut("update")]
+    public IActionResult Update([FromBody] Trainee trainee)
+    {
+        dataAccessLayer.Update(trainee);
+        return Ok("Updated");
+    }
+
+    [HttpDelete("delete")]
+    public IActionResult Delete([FromQuery] string email)
+    {
+        dataAccessLayer.Delete(email);
+        return Ok("Deleted");
+    }
+
+    [HttpGet("get")]
+    public IActionResult GetByEmail([FromQuery] string email)
+    {
+        var trainee = dataAccessLayer.GetByEmail(email);
+        if (trainee == null)
+        {
+            return NotFound("Trainee not found");
+        }
+        return Ok(trainee);
+    }
+}
