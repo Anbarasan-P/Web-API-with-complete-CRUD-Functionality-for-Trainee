4f94a741dd42d3ebff07c245f652b7382a6543a5
Anbarasan-P <anbarasanpandikumar@gmail.com>
2025-08-01 14:05:24 +0530

    Added Trainee and Login models

---
+++ Trainee-webAPI/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Trainee_webAPI.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+
+}
---
+++ Trainee-webAPI/Models/Trainee.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Trainee_webAPI.Models
+
+{
+    public class Trainee
+    {
+        public int TraineeID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Department { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Gender { get; set; }
+        public byte[] Photo { get; set; }
+        public string Password { get; set; }
+    }
+
+
+}
